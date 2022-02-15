c3fc33a1301d955d51bcdb3b116edea778675f2d
marta <marta@LAPTOP-AOKGKF5C>
2022-02-15 17:04:49 +0100

    añado paquetes

---
+++ src/servidor/Servidor.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package servidor;
+
+/**
+ * @author marta
+ *
+ */
+public class Servidor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
