bd8e278b96d7237319f38cf24442773ec0f0d2cd
marta <marta@LAPTOP-AOKGKF5C>
2022-02-18 15:37:40 +0100

    c

--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -3,17 +3,51 @@
  */
 package servidor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
 /**
  * @author marta
  *
  */
 public class Servidor {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int puerto = 13000;
+		System.setProperty("javax.net.ssl.keystore", "src/certificado/mpd");
+		System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+		
+		try {
+			SSLServerSocketFactory fabrica = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+			SSLServerSocket miServidor = (SSLServerSocket) fabrica.createServerSocket(puerto);
+			
+			//ServerSocket miServidor = new ServerSocket(puerto);
+			System.out.println("SERVIDOR: Escuchando por el puerto " + puerto);
+			miServidor.setSoTimeout(0);//espera infinito
+			SSLSocket cliente = (SSLSocket) miServidor.accept();//produce un bloqueo esperando peticiones
+			System.out.println("se ha conectado, esperando entrada Cliente");
+			
+			// Para leer lo que llegue por el nuevo socket
+			InputStream lecturaSocket = cliente.getInputStream();
+			InputStreamReader lector = new InputStreamReader(lecturaSocket);
+			BufferedReader leeCliente = new BufferedReader(lector);
+			
+			String linea = leeCliente.readLine();
+			System.out.println("He leido =>" + linea);
+			 
+			// cierro todos los recursos
+			leeCliente.close();
+			miServidor.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	}
 
